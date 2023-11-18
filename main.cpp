1873b86063b5ed2f93ad7f8bfe60d9df9b40ba24
Vladislav Balabaev <vv.balabaev@gmail.com>
2023-11-18 16:44:49 +0300

    hello world

---
+++ main.cpp
@@ -0,0 +1,8 @@
+# include <iostream>                    // input-output stream
+
+int main() {                            // returns integer; random if smth goes wrond
+    // std - standard (library)
+    std::cout << "Hello World!";        // character output
+    return 0;                           // to show that it terminates correctly
+}
+// to compile: $ g++ main.cpp
