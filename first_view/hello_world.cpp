aeb69357017ff3487fcdd9077396915208d18ebc
Vladislav Balabaev <vv.balabaev@gmail.com>
2023-11-20 14:00:08 +0300

    transfer comments to chapter

--- first_view/hello_world.cpp
+++
@@ -1,8 +0,0 @@
-# include <iostream>                    // input-output stream
-
-int main() {                            // returns integer; random if smth goes wrond
-    // std - standard (library); << - stream instertion operator
-    std::cout << "Hello World!" << std::endl;        // character output
-    return 0;                           // to show that it terminates correctly
-}
-// to compile: $ g++ hello_world.cpp
