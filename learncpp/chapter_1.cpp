61720e74c90b2e68e572f499259b7ba6ad8932e8
Vladislav Balabaev <vv.balabaev@gmail.com>
2023-11-21 21:08:36 +0300

    update chapters

--- learncpp/chapter_1.cpp
+++ learncpp/chapter_1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>                                     // Line 1 - a preprocessor directive; input-output stream.
 /*
 "statement" -> ;
+statements in C++ are executed sequentially
 
 "identiifier" - name of smth (variable, function, ...)
 
@@ -14,20 +15,28 @@ C++ is statically typed language (when declaring a variable we have to specify t
 
 "instantiation" of an object - the object will be created and assigned a memory address while "runtime"
 "instance" - an instantiated object
+
+
+Undefined behavior (UB) is the result of executing code whose behavior is not well-defined by the C++ language,
+for example - uninitialized variable
+
+Literals are values that are inserted directly into the source code.
+
+expression & evaluation & result
 */
 
 
 int main() {                                                // returns integer; random if smth goes wrond
-    int x;                                                  // "definition", declaration?
+    int x;                                                  // "definition"
     x = 1;                                                  // "assignment"
 
     // types of "initialization":
     int a;                                                  // default
     int b = 1;                                              // copy
     int c( 1 );                                             // direct - efficient initialization of complex objects
-    int d { 1 };                                            // direct list (brace, uniform)
+    int d{ 1 };                                            // direct list (brace, uniform)
     int e = { 1 };                                          // copy list
-    int f {};                                               // value
+    int f{ };                                               // value
     /* preffered way - direct list or value initialization:
     1. disallows “narrowing conversions”: 'int {0.1}' is a error 
     2. supports initialization with lists of values
@@ -37,12 +46,21 @@ int main() {                                                // returns integer; 
 
     [[maybe_unused]] int y { 2 };                           // to avoid compile error of unused variable.
 
-    // std - standard (library); << - stream instertion operator
+    /*
+    std - standard (library); << - stream instertion operator
+    std::cout is buffered; if program crashes, aborts, paused before the buffer is flushed, any output still waiting in the buffer will not be displayed.
+    Writing data to a buffer is fast, whereas transferring a batch of data to an output device is comparatively slow.
+    std::endl flushes the buffer -> 
+    
+    Best practice - prefer ‘\n’ over std::endl
+    */
     std::cout << "Hello World!" << std::endl;        // character output
 
-    int value_1;
-    int value_2;
+    int value_1{ };
+    int value_2{ };
     std::cin >> value_1 >> value_2;                         // >> - stream extraction operator
-
-    return 0;                           // to show that it terminates correctly
+ 
+    return 0;                           // "status code" - to show that it terminates correctly
+    // only 3 status codes: 0 = EXIT_SUCCESS, EXIT_FAILURE
 }
+// to compile: $ g++ chapter_1.cpp
---
+++ learncpp/chapter_2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+/*
+"function call" is an expression that tells the CPU to interrupt the current function and execute another function
+
+functions cannot be defined inside other functions
+
+"local scope" & "out of scope"
+out of scope -> variable’s lifetime ends;; not all types of variables are destroyed when they go out of scope
+
+
+"declaration" - existence of an identifier and its associated type information.
+"definition" - declaration that actually implements (for functions and types) or instantiates (for variables) the identifier. [In C++, all definitions are declarations.]
+"pure declarations" - declarations that aren’t definitions are called.
+"initialization" - provides an initial value for a defined object.
+*/
+
+
+// void func. automatically return to the caller at the end of the function
+void do_print(int x) {                                   // func. "header"
+    std::cout << "Printing" << x << "\n";                // func. "body"
+}
+
+
+double function_name(int x, int y)
+{
+    // failure to return a value from a value-returning function -> undefined behavior.
+    return x + y;
+}
+
+
+void func_with_unnamed_parameter(int /*previously_used_parameter*/) {
+    // in cases where a function parameter needs to exist but is not used in the body of the function
+}
+
+
+int forward_declared(int x, int y);                     // "forward declaration"
+// it may be declared without names of parameters, but it's common practice to add them.
+// forward declaration is good when we have two functions that call each other.
+// we can have smth.cpp with defined forward_declared and use forward declaration to use it.
+
+
+int main() {
+    std::cout << forward_declared(2, 4) << '\n';
+}
+
+
+int forward_declared(int x, int y) {
+    return x + y;
+}
+
+// $ g++ main.cpp add.cpp -o main, where main.cpp and add.cpp are the names of your code files, and main is the name of the output file.
